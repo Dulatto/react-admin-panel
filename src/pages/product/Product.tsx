828c1c8c020f6a6b5c1eedb66830bfc320ea5804
Dulatto <dulat.omarov@gmail.com>
2024-06-13 22:25:22 -0600

    single

---
+++ src/pages/product/Product.tsx
@@ -0,0 +1,11 @@
+import { Single } from '../../components/single/Single';
+import './product.scss';
+
+export const Product = () => {
+    //Fetch data and send to Single component
+    return (
+        <div className="product">
+            <Single />
+        </div>
+    );
+};
---
+++ src/pages/user/User.tsx
@@ -0,0 +1,11 @@
+import { Single } from '../../components/single/Single';
+import './user.scss';
+
+export const User = () => {
+    //Fetch data and send to Single component
+    return (
+        <div className="user">
+            <Single />
+        </div>
+    );
+};
